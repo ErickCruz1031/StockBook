bc90b3eba2c171bf783143dfb140bfa6869eed1b
erickcruz1031@gmail.com <erickcruz1031@gmail.com>
2020-10-25 17:29:40 -0700

    Added primary API functionality to fetch company information

--- StockBook/book-frontend/src/App.js
+++ StockBook/book-frontend/src/App.js
@@ -16,12 +16,35 @@ function App() {
               <input id = "searchInput" type="text" placeholder="Stock Ticker.."/>
             </div>
             <div id="buttonWrapper">
-              <button type="button"> Search </button>
+              <button type="button" onClick={activateSearch}> Search </button>
             </div> 
           </div>
         </div> 
     </div>
   );
+}
+
+async function activateSearch(){
+  const baseURL = "https://api.polygon.io/v1/meta/symbols/" //Base URL for the call 
+  const apiKey = "EwdgXn2W7ptj4vkx9B40T3HiVEvV4v3e"
+  //"https://api.polygon.io/v1/meta/symbols/AAPL/company?apiKey=EwdgXn2W7ptj4vkx9B40T3HiVEvV4v3e"
+
+  console.log("Clicked the search button")
+  
+  var ticker = document.getElementById("searchInput").value.toUpperCase();
+  console.log("The input was " + ticker)
+
+  const req = baseURL + ticker + "/company?apikey=" + apiKey;
+  console.log("We are going to call it with this URL:"+ req);
+
+  const response = await fetch(req);
+  const data = await response.json();
+  console.log(data);
+
+
+
+
+
 }
 
 
