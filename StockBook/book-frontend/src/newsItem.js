40660c12284e91de369a0993ac622fa69a87f714
erickcruz1031@gmail.com <erickcruz1031@gmail.com>
2020-12-10 22:10:03 -0800

    Added the ability to add the images of the articles to the newsItem. Installed the NPM library to fit images to the containers

--- StockBook/book-frontend/src/newsItem.js
+++ StockBook/book-frontend/src/newsItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Div, Icon, Input, Image, Text} from "react-atomize";
+import FittedImage from 'react-fitted-image';
 
 
 export class NewsItem extends Component{
@@ -7,15 +8,22 @@ export class NewsItem extends Component{
       componentDidMount(){
         console.log("This component was mounted. Calling the API");
         var newsURL = 'https://api.polygon.io/v1/meta/symbols/' + this.state.ticker + '/news?perpage=5&page=1&apiKey=EwdgXn2W7ptj4vkx9B40T3HiVEvV4v3e'
+        var logoURL = 'https://api.polygon.io/v1/meta/symbols/' + this.state.ticker + '/company?apiKey=EwdgXn2W7ptj4vkx9B40T3HiVEvV4v3e'
         fetch(newsURL)
             .then(response => response.json())
-            .then(data => this.setState({ headline: data[0].title, summary: data[0].summary}));
+            .then(data => this.setState({ headline: data[0].title, summary: data[0].summary, url:data[0].image}));
+/*
+        fetch(logoURL)
+            .then(response => response.json())
+            .then(data => this.setState({ url: data.logo}));
+            */
     
       }
 
       constructor(props){  
         super(props);  
-        this.state = {  
+        this.state = {
+            url : "null",  
             summary : "null",
             headline : "null", 
             ticker : props.stock 
@@ -29,15 +37,16 @@ export class NewsItem extends Component{
       return (
 
 
-        //We're going to change the Logo to an actual logo from the atomize library
+        //We're going to change the Logo to an actual logo from the atomize library <Icon name="Link" color="black" size="40%"/>
         <Div className="ElementOne" w="100%" h="20%" d="flex"> 
-            <Div className="ElementTwo" w="10%" h="100%" align="center"> 
-                <Icon name="Link" color="black" size="40%"/>
+            <Div className="ElementTwo" w="10%" h="100%" align="center" border="1px solid" borderColor="gray600" rounded="xl" bg="gray400" hoverBg="gray900"> 
+                <FittedImage fit="contain" loader={<div> Loading </div>}  src={this.state.url} rounded="xl"/>
             </Div>
-            <Div className="Item-Desc" w="85%" h="100%" border="1px solid" borderColor="gray600" rounded="xl" d="flex" flexDir="column" align="center"> 
+            <Div className="Item-Desc" w="90%" h="100%" border="1px solid" borderColor="gray600" rounded="xl" d="flex" flexDir="column" align="center"> 
                 <Text textWeight="700" fontFamily="secondary" textAlign="center" textSize="heading"> {this.state.headline} </Text>
                 <Text textWeight="500" fontFamily="secondary" textAlign="center" textSize="title"> {this.state.summary} </Text>
             </Div>
+
         </Div>
 
   
