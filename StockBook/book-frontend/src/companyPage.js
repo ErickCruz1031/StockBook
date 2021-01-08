cd77c6faf9417bbc71737402bd3e681baa9d3511
erickcruz1031@gmail.com <erickcruz1031@gmail.com>
2021-01-07 22:36:22 -0800

    Update to latest version

--- StockBook/book-frontend/src/companyPage.js
+++ StockBook/book-frontend/src/companyPage.js
@@ -13,7 +13,7 @@ export class CompanyPage extends Component{
         //Might have to play with the flex direction of this component
         <Div className="Company-Information" w="100%" h="100%" d="flex" flexDir="column">
             <Div w="100%" h="15%" d="flex" flexDir="column">
-                <Text textSize="display2" w="100%" h="50%" textAlign="center"> Tesla </Text>
+                <Text textSize="display2" w="100%" h="50%" textAlign="center" textWeight="800"> Tesla </Text>
                 <Div className="ElementTwo" w="100%" h="50%" align="center" rounded="xl"> 
                     <FittedImage fit="contain" loader={<div> Loading </div>}  src="https://s3.polygon.io/logos/tsla/logo.png" rounded="xl"/>
                 </Div>
@@ -24,23 +24,23 @@ export class CompanyPage extends Component{
 
                 <Div className="Info-ColOne" w="50%" h="100%">
                     <Div className="data-point" p={{b:"5%"}} h="20%">
-                        <Text textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
-                        <Text textSize="display1" textWeight="500" textAlign="center"> Value </Text>
+                        <Text fontFamily="secondary" textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
+                        <Text fontFamily="primary" textSize="display1" textWeight="500" textAlign="center"> Value </Text>
                     </Div>
 
                     <Div className="data-point" p={{b:"5%"}} h="20%">
-                        <Text textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
-                        <Text textSize="display1" textWeight="500" textAlign="center"> Value </Text>
+                        <Text fontFamily="secondary" textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
+                        <Text fontFamily="primary" textSize="display1" textWeight="500" textAlign="center"> Value </Text>
                     </Div>
 
                     <Div className="data-point" p={{b:"5%"}} h="20%">
-                        <Text textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
-                        <Text textSize="display1" textWeight="500" textAlign="center"> Value </Text>
+                        <Text fontFamily="secondary" textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
+                        <Text fontFamily="primary" textSize="display1" textWeight="500" textAlign="center"> Value </Text>
                     </Div>
 
                     <Div className="data-point" p={{b:"5%"}} h="20%">
-                        <Text textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
-                        <Text textSize="display1" textWeight="500" textAlign="center"> Value </Text>
+                        <Text fontFamily="secondary" textSize="display1" textWeight="700" textAlign="center"> Section Title </Text>
+                        <Text fontFamily="primary" textSize="display1" textWeight="500" textAlign="center"> Value </Text>
                     </Div>
 
                 </Div>
